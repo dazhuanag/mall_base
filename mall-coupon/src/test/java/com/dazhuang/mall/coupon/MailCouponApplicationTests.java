2e519c01071fc1c83e7c23d8531e2116e2eb8c9d
zhuangazhuang <1743460289@qq.com>
2021-08-02 12:27:50 +0800

    init

---
+++ mall-coupon/src/test/java/com/dazhuang/mall/coupon/MailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazhuang.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class mallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mall-ware/src/test/java/com/dazhuang/mall/ware/MailWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dazhuang.mall.ware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class mallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
